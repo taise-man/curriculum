f06ecca4cf16be5a81d214ceb89db07fa1e0d80f
taise-man <96433983+taise-man@users.noreply.github.com>
2022-01-13 06:55:20 +0900

    課題提出
    3章のチェックテスト

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+public class Main{
+    public static void main(String[] args){
+
+        
+        int[] data = {3,1,2,7,5};
+        
+
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = i + 1; j > i; j--) {
+
+                if(data[i] > data[j]){
+                  int box = data[i];
+                  data[i] = data[j];
+                  data[j] = box;
+                }
+            }
+        }
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+    }
+}
+
